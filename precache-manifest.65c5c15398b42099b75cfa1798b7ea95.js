fb61a7d2045a88ad0a6b6c1b0d680470cb5863e4
myangaa1 <m.myanganbayar@gmail.com>
2020-09-06 12:35:49 +0800

    Updates

---
+++ precache-manifest.65c5c15398b42099b75cfa1798b7ea95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0de3bddb8252ba5a9c1f7b304c617c3e",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "bb4127cde10dbcc4594e",
+    "url": "/burger/static/css/main.038faa21.chunk.css"
+  },
+  {
+    "revision": "d8344b7e613fa04156e0",
+    "url": "/burger/static/js/2.a7103e70.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/burger/static/js/2.a7103e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb4127cde10dbcc4594e",
+    "url": "/burger/static/js/main.f6f28c1e.chunk.js"
+  },
+  {
+    "revision": "d4390a2908c1cac1ad60",
+    "url": "/burger/static/js/runtime-main.b5950f3a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
